0aa9d31cd0bc8d5dab6e4429c11306f17f0841e2
Seven Ecks <sevenecks@gmail.com>
2025-09-25 12:08:17 -0400

    Remove GitHub Actions workflow

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import globals from 'globals';
+
+const baseConfig = {
+  files: ['**/*.js', '**/*.jsx'],
+  ...js.configs.recommended,
+  languageOptions: {
+    ...js.configs.recommended.languageOptions,
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true
+      }
+    },
+    globals: {
+      ...globals.node
+    }
+  },
+  rules: {
+    ...js.configs.recommended.rules,
+    ...prettier.rules,
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }]
+  }
+};
+
+export default [
+  {
+    ignores: ['node_modules/', 'public/', 'client/', '*.sqlite']
+  },
+  baseConfig,
+  {
+    files: ['**/__tests__/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+        ...globals.node
+      }
+    }
+  }
+];
