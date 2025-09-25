30a6d0118a031c44f113026655e6383ccbfcc209
Seven Ecks <sevenecks@gmail.com>
2025-09-25 11:07:54 -0400

    Replace ad-hoc schema setup with migrations

---
+++ seeds/001_default_admin.cjs
@@ -0,0 +1,17 @@
+/**
+ * @param {import('knex')} knex
+ */
+exports.seed = async function seed(knex) {
+  const existingAdmin = await knex('users')
+    .where({ Email: 'admin@nodervisor' })
+    .first();
+
+  if (!existingAdmin) {
+    await knex('users').insert({
+      Name: 'Admin',
+      Email: 'admin@nodervisor',
+      Password: '$2a$10$OI5bfzPATM2358vQlDYKweliWYI2FyJwqsDJUMXuqaSzM.7vNa3xu',
+      Role: 'Admin'
+    });
+  }
+};
